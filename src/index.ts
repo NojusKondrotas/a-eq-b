27f9c39ec87de0a2056298871a6366ae120229cc
NojusKondrotas <nojus.kondrotas23@gmail.com>
2026-04-24 20:19:57 +0300

    Automate configuring of sort page links

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,18 @@
 import { hash } from "./numerics.ts";
 
 document.addEventListener('DOMContentLoaded', () => {
-    const mergeSort = document.getElementById('merge-sort');
-    const insertionSort = document.getElementById('insertion-sort');
+    const sorts = ['merge', 'insertion', 'selection', 'quick', 'bubble', 'heap'];
+    sorts.forEach(sort => {
+        const sortClickerDOM = document.getElementById(`${sort}-sort`);
+        const sortHashDOM = document.getElementById(`${sort}-sort-hash`);
+        const sortLabelDOM = document.getElementById(`${sort}-label`);
 
-    const mergeSortHash = document.getElementById('merge-sort-hash');
-    const insertionSortHash = document.getElementById('insertion-sort-hash');
+        if (!sortClickerDOM || !sortHashDOM || !sortLabelDOM) {
+            window.location.href = 'pages/error/error.html';
+            return;
+        }
 
-    if (!mergeSort || !insertionSort || !mergeSortHash || !insertionSortHash) {
-        window.location.href = 'pages/error/error.html';
-        return;
-    }
-
-    mergeSortHash.textContent = hash('merge sort');
-    insertionSortHash.textContent = hash('insertion sort');
-
-    mergeSort.addEventListener('click', () => sessionStorage.setItem('selected_sort', 'merge'));
-    insertionSort.addEventListener('click', () => sessionStorage.setItem('selected_sort', 'insertion'));
-})
+        sortHashDOM.textContent = hash(sortLabelDOM.textContent);
+        sortClickerDOM.addEventListener('click', () => sessionStorage.setItem('selected_sort', sort));
+    });
+});
