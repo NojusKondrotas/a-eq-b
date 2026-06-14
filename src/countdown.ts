a20f2b39041fef35bf5796dcc4f4dd8af276ed04
NojusKondrotas <nojus.kondrotas23@gmail.com>
2026-06-14 18:19:41 +0300

    Trust DOM element existence validation to called functions, encapsulate stats' measuring

--- src/countdown.ts
+++ src/countdown.ts
@@ -1,11 +1,10 @@
-import { getArrSize, getMeasurement, positionStats, setConfigsDisplay, setCountdownDisplay, setFooterDisplay, setReducedFooterDisplay, setSortPlaygroundDisplay, setSortStatsDisplay } from "./configs.ts";
-import { initElementMeasurement, MeasureLine } from "./utils/dom_measurer.ts";
-import { deleteChildren } from "./minigames/merge_sort/draw.ts";
+import { getArrSize, getMeasurement, measureSortStats, reinitInitArr, setConfigsDisplay, setCountdownDisplay, setFooterDisplay, setReducedFooterDisplay, setSortPlaygroundDisplay, setSortStatsDisplay } from "./configs.ts";
 import { shuffle } from "./utils/numerics.ts";
 import { startMergeSort } from "./sorts/merge/merge_handler.ts";
 import { AsymptoticNotations, Complexity, initSortLog, logArr, logTheoreticalComparisons, logTotalComparisons } from "./sorts/sort_logger.ts";
 import { SortType } from "./sorts/sort_types.ts";
 import { initAbortController } from "./utils/time-event-handler.ts";
+import { startInsertionSort } from "./sorts/insertion/insertion_handler.ts";
 
 export const sleep = (ms: number, signal?: AbortSignal): Promise<void> =>
     new Promise((resolve, reject) => {
@@ -20,30 +19,14 @@ export const sleep = (ms: number, signal?: AbortSignal): Promise<void> =>
 export const drawCountdown = (cnt: HTMLElement, sec: number) => cnt.innerHTML = `${sec}`;
 
 export async function countdown(type: SortType, secs: number) {
-    const configurations = document.getElementById('configurations');
-    const startGame = document.getElementById('start-game');
-    const guideText = document.getElementById('guide-text');
-    const countdownCnt = document.getElementById('countdown-cnt');
     const countdownCntText = document.getElementById('countdown-cnt-text');
-    const playground = document.getElementById('playground');
 
-    const stats = document.getElementById('stats');
-    const footer = document.getElementById('footer');
-    const reducedFooter = document.getElementById('reduced-footer');
-    const totalComps = document.getElementById('total-comps');
-    const omega = document.getElementById('omega');
-    const theta = document.getElementById('theta');
-    const bigO = document.getElementById('bigo');
-    const initArrDOM = document.getElementById('init-arr');
-
-    if (!configurations || !startGame || !guideText || !countdownCnt || !countdownCntText || !playground
-        || !stats || !footer || !reducedFooter || !totalComps || !omega || !theta || !bigO || !initArrDOM
-    ) {
+    if (!countdownCntText) {
         window.location.href = "pages/error/error.html";
         return;
     }
 
-    deleteChildren(initArrDOM);
+    reinitInitArr();
 
     setConfigsDisplay('none');
     setFooterDisplay('none');
@@ -78,30 +61,16 @@ export async function countdown(type: SortType, secs: number) {
             await startMergeSort(arrInit);
             break;
         case SortType.InsertionSort:
-            await startMergeSort(arrInit);
+            await startInsertionSort(arrInit);
             break;
     }
 
-    setReducedFooterDisplay('grid');
     setSortStatsDisplay('flex');
-    positionStats(reducedFooter, stats);
+    setReducedFooterDisplay('grid');
 
-    logTotalComparisons(totalComps);
-    logTheoreticalComparisons(arrInit.length, new AsymptoticNotations(Complexity.nlogn, Complexity.nlogn, Complexity.nlogn),
-        omega, theta, bigO
-    );
-    logArr(arrInit, initArrDOM);
+    logTotalComparisons();
+    logTheoreticalComparisons(arrInit.length, new AsymptoticNotations(Complexity.nlogn, Complexity.nlogn, Complexity.nlogn));
+    logArr(arrInit);
 
-    const totalCompsMes = initElementMeasurement(totalComps, document.body, 0, 0, 0,
-        [MeasureLine.Top, MeasureLine.Right, MeasureLine.Bottom, MeasureLine.Left]);
-    const omegaMes = initElementMeasurement(omega, document.body, 0, 0, 0,
-        [MeasureLine.Top, MeasureLine.Right, MeasureLine.Bottom, MeasureLine.Left]);
-    const thetaMes = initElementMeasurement(theta, document.body, 0, 0, 0,
-        [MeasureLine.Top, MeasureLine.Right, MeasureLine.Bottom, MeasureLine.Left]);
-    const bigOMes = initElementMeasurement(bigO, document.body, 0, 0, 0,
-        [MeasureLine.Top, MeasureLine.Right, MeasureLine.Bottom, MeasureLine.Left]);
-    totalCompsMes.setDisplay('none');
-    omegaMes.setDisplay('none');
-    thetaMes.setDisplay('none');
-    bigOMes.setDisplay('none');
+    measureSortStats();
 }
